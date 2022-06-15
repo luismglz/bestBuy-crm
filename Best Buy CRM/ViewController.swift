41b3453895eaa7aedd2f57a637c42c0d94030822
Luis GL <lgl66505@udelasalle.edu.mx>
2022-06-14 23:47:13 -0500

    add empty project

---
+++ Best Buy CRM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Best Buy CRM
+//
+//  Created by Luis GL on 14/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
